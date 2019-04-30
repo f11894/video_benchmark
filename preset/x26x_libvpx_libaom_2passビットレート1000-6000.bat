e6f7f05b2fda651ac05e8aba81e9ec74078e94a9
fg118942 <ekusuy-s@jcom.home.ne.jp>
2019-04-30 14:10:06 +0900

    libaomのコマンドラインオプションを変更

--- preset/x26x_libvpx_libaom_2passビットレート1000-6000.bat
+++ preset/x26x_libvpx_libaom_2passビットレート1000-6000.bat
@@ -2,7 +2,7 @@
 cd "%~dp0"
 for %%i in ("..\video_benchmark.bat") do set benchmark_bat="%%~dpnxi"
 
-for /L %%i in (1000,1000,6000) do call %benchmark_bat% "%~1" "%~n1_libaom_c0_%%ikbps.mp4" "--ivf --cpu-used=0 --tile-columns=3 --tile-rows=3 --end-usage=vbr --passes=2 --target-bitrate=%%i --kf-max-dist=250" libaom c0_2pass_bitrate
+for /L %%i in (1000,1000,6000) do call %benchmark_bat% "%~1" "%~n1_libaom_c0_%%ikbps.mp4" "--ivf --cpu-used=0 --threads=8 --tile-columns=2 --tile-rows=2 --end-usage=vbr --passes=2 --target-bitrate=%%i --kf-max-dist=250" libaom c0_2pass_bitrate
 for /L %%i in (1000,1000,6000) do call %benchmark_bat% "%~1" "%~n1_x264_placebo_tunessim_%%ikbps.mp4" "--preset placebo --tune ssim --bitrate %%i --pass 2" x264 placebo_tunessim_2pass_bitrate
 for /L %%i in (1000,1000,6000) do call %benchmark_bat% "%~1" "%~n1_x265_placebo_tunessim_%%ikbps.mp4" "--preset placebo --tune ssim --bitrate %%i --pass 2" x265 placebo_tunessim_2pass_bitrate
 for /L %%i in (1000,1000,6000) do call %benchmark_bat% "%~1" "%~n1_libvpx_vp9_c0_2pass_crf%%i.webm" "--codec=vp9 --frame-parallel=0 --tile-columns=2 --good --cpu-used=0 --tune=psnr --passes=2 --threads=2 --end-usage=vbr --target-bitrate=%%i --webm --auto-alt-ref=6 --kf-max-dist=250" libvpx vp9_c0_2pass_bitrate
--- preset/x26x_libvpx_libaom_crf.bat
+++ preset/x26x_libvpx_libaom_crf.bat
@@ -8,7 +8,7 @@ for /L %%i in (32,-2,18) do call %benchmark_bat% "%~1" "%~n1_x265_placebo_tuness
 
 for /L %%i in (55,-5,25) do call %benchmark_bat% "%~1" "%~n1_libvpx_vp9_c0_2pass_q%%i.webm" "--codec=vp9 --frame-parallel=0 --tile-columns=2 --good --cpu-used=0 --tune=psnr --passes=2 --threads=2 --end-usage=q --cq-level=%%i --webm --auto-alt-ref=6 --kf-max-dist=250" libvpx vp9_c0_2pass_q
 
-for /L %%i in (55,-5,25) do call %benchmark_bat% "%~1" "%~n1_libaom_c0_2pass_q%%i.mp4" "--ivf --cpu-used=0 --threads=8 --tile-columns=3 --tile-rows=3 --passes=2 --end-usage=q --cq-level=%%i --kf-max-dist=250" libaom c0_2pass_q
+for /L %%i in (55,-5,25) do call %benchmark_bat% "%~1" "%~n1_libaom_c0_2pass_q%%i.mp4" "--ivf --cpu-used=0 --threads=8 --tile-columns=2 --tile-rows=2 --passes=2 --end-usage=q --cq-level=%%i --kf-max-dist=250" libaom c0_2pass_q
 
 rem 10bit—pƒTƒ“ƒvƒ‹
 rem for /L %%i in (50,-2,30) do call %benchmark_bat% "%~1" "%~n1_SVT-AV1_10bit_e0_q%%i.mp4" "-enc-mode 0 -q %%i -bit-depth 10" SVT-AV1 10bit_e0 10bit
@@ -16,5 +16,5 @@ rem for /L %%i in (50,-2,30) do call %benchmark_bat% "%~1" "%~n1_SVT-AV1_10bit_e
 rem for /L %%i in (32,-2,18) do call %benchmark_bat% "%~1" "%~n1_x264_10bit_placebo_tunessim_crf%%i.mp4" "--preset placebo --tune ssim --crf %%i --input-depth 10 --output-depth 10" x264 10bit_placebo_tunessim_crf 10bit
 rem for /L %%i in (32,-2,18) do call %benchmark_bat% "%~1" "%~n1_x265_10bit_placebo_tunessim_crf%%i.mp4" "--preset placebo --tune ssim --crf %%i --input-depth 10 --output-depth 10" x265 10bit_placebo_tunessim_crf 10bit
 
-rem for /L %%i in (55,-5,25) do call %benchmark_bat% "%~1" "%~n1_libaom_10bit_c0_2pass_q%%i.mp4" "--ivf --cpu-used=0 --threads=8 --tile-columns=3 --tile-rows=3 --passes=2 --end-usage=q --cq-level=%%i --kf-max-dist=250 --input-bit-depth=10 --bit-depth=10" libaom 10bit_c0_2pass_q 10bit
+rem for /L %%i in (55,-5,25) do call %benchmark_bat% "%~1" "%~n1_libaom_10bit_c0_2pass_q%%i.mp4" "--ivf --cpu-used=0 --threads=8 --tile-columns=2 --tile-rows=2 --passes=2 --end-usage=q --cq-level=%%i --kf-max-dist=250 --input-bit-depth=10 --bit-depth=10" libaom 10bit_c0_2pass_q 10bit
 exit /b
