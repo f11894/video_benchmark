9e49e74b3579e926387dd8fafadf1f409adeecf7
fg118942 <ekusuy-s@jcom.home.ne.jp>
2019-05-20 18:51:23 +0900

    小数の桁数を多くして精度を上げる

--- ffmediaInfo.bat
+++ ffmediaInfo.bat
@@ -9,9 +9,9 @@ FOR /f "tokens=4 DELIMS='" %%i IN ("%frame_rate%") DO SET frame_rate=%%i
 FOR /f "tokens=1 DELIMS=/" %%i IN ("%frame_rate%") DO SET frame_rate_num=%%i
 FOR /f "tokens=2 DELIMS=/" %%i IN ("%frame_rate%") DO SET frame_rate_denom=%%i
 set frame_rate_mp4box=%frame_rate_num%
-if "%frame_rate%"=="60000/1001" set frame_rate_mp4box=59.940
-if "%frame_rate%"=="30000/1001" set frame_rate_mp4box=29.970
-if "%frame_rate%"=="24000/1001" set frame_rate_mp4box=23.976
+if "%frame_rate%"=="60000/1001" set frame_rate_mp4box=59.940060
+if "%frame_rate%"=="30000/1001" set frame_rate_mp4box=29.970030
+if "%frame_rate%"=="24000/1001" set frame_rate_mp4box=23.976025
 FOR /f "tokens=4 DELIMS='" %%i IN ("%video_size%") DO SET video_size=%%i
 FOR /f "tokens=1 DELIMS=x" %%i IN ("%video_size%") DO SET Width=%%i
 FOR /f "tokens=2 DELIMS=x" %%i IN ("%video_size%") DO SET Height=%%i
