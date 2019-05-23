1445f8b120c4fb1bcc6c92417748d58864f06710
fg118942 <ekusuy-s@jcom.home.ne.jp>
2019-05-23 18:33:37 +0900

    Create_Graph.pyをexe化する
    Pythonをインストールしてなくても動作するように

--- Create_Graph.bat
+++ Create_Graph.bat
@@ -1,10 +1,8 @@
 @echo off
 rem benchmark_logフォルダをドロップして使う
-rem 予めPythonをインストールしてあることが前提
 
 cd "%~1"
 echo グラフを作成しています
-Python "%~dp0Create_Graph.py" "%~nx1" 8bit
-Python "%~dp0Create_Graph.py" "%~nx1" 10bit
+"%~dp0tools\Create_Graph.exe" "%~1"
 echo グラフの作成が終了しました
 pause
