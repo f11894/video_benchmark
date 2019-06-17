080c7054e5e48523c15442fd2e5a601402789218
fg118942 <ekusuy-s@jcom.home.ne.jp>
2019-06-17 19:45:14 +0900

    入力ファイルの情報を調べる処理を変更
    一時ファイルを作らないようにする

--- ffmediaInfo.bat
+++ ffmediaInfo.bat
@@ -2,9 +2,8 @@
 set ffmpeg="%~dp0tools\ffmpeg.exe"
 set mediaInfo="%~dp0tools\MediaInfo.exe"
 
-%ffmpeg% -loglevel 48 -i "%~1" -t 00:00:00.00 -vcodec utvideo -an -f null - >"%TEMP%\ffmediaInfo.txt"  2>&1
-FOR /f "DELIMS=" %%i IN ('find "'frame_rate'" "%TEMP%\ffmediaInfo.txt"') DO SET frame_rate=%%i
-FOR /f "DELIMS=" %%i IN ('find "'video_size'" "%TEMP%\ffmediaInfo.txt"') DO SET video_size=%%i
+pushd "%~dp0tools\"
+FOR /f "DELIMS=" %%i IN ('ffmpeg.exe -loglevel 48 -i "%~1" -t 00:00:00.00 -vcodec utvideo -an -f null -  2^>^&1 ^| find "'frame_rate'"') DO SET frame_rate=%%i
 FOR /f "tokens=4 DELIMS='" %%i IN ("%frame_rate%") DO SET frame_rate=%%i
 FOR /f "tokens=1 DELIMS=/" %%i IN ("%frame_rate%") DO SET frame_rate_num=%%i
 FOR /f "tokens=2 DELIMS=/" %%i IN ("%frame_rate%") DO SET frame_rate_denom=%%i
@@ -22,12 +21,10 @@ if "%frame_rate%"=="24000/1001" (
    set frame_rate_mp4box=23.976025
    set frame_rate_integer=24
 )
-FOR /f "tokens=4 DELIMS='" %%i IN ("%video_size%") DO SET video_size=%%i
-FOR /f "tokens=1 DELIMS=x" %%i IN ("%video_size%") DO SET Width=%%i
-FOR /f "tokens=2 DELIMS=x" %%i IN ("%video_size%") DO SET Height=%%i
-del "%TEMP%\ffmediaInfo.txt"
-pushd "%~dp0tools\"
 FOR /f "DELIMS=" %%i IN ('MediaInfo.exe --Inform^=Video^;%%Duration/String3%% "%~1"') DO SET Duration=%%i
 FOR /f "DELIMS=" %%i IN ('MediaInfo.exe --Inform^=Video^;%%FrameCount%% "%~1"') DO SET FrameCount=%%i
+FOR /f "DELIMS=" %%i IN ('MediaInfo.exe --Inform^=Video^;%%Width%% "%~1"') DO SET Width=%%i
+FOR /f "DELIMS=" %%i IN ('MediaInfo.exe --Inform^=Video^;%%Height%% "%~1"') DO SET Height=%%i
+set video_size=%Width%x%Height%
 popd
 exit /b
