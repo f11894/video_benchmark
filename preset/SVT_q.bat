330a07b05c5b80ebb110516646c4abf65b2c96c1
fg118942 <ekusuy-s@jcom.home.ne.jp>
2019-06-21 19:38:32 +0900

    SVT-VP9のコメントアウトを消す

--- preset/SVT_q.bat
+++ preset/SVT_q.bat
@@ -5,9 +5,7 @@ for %%i in ("..\video_benchmark.bat") do set benchmark_bat="%%~dpnxi"
 if "%~1"=="" goto end
 for /L %%i in (32,-2,18) do call %benchmark_bat% "%~1" "%~n1_SVT-HEVC_encmode0_q%%i.mp4" "-encMode 0 -profile 1 -q %%i" SVT-HEVC encmode0
 for /L %%i in (50,-3,26) do call %benchmark_bat% "%~1" "%~n1_SVT-AV1_encmode0_q%%i.mp4" "-enc-mode 0 -intra-period 63 -q %%i" SVT-AV1 encmode0
-
-rem SVT-VP9‚Í‚Ü‚¾ˆÀ’è‚µ‚Ä‚¢‚È‚¢
-rem for /L %%i in (50,-3,26) do call %benchmark_bat% "%~1" "%~n1_SVT-VP9_encmode0_q%%i.webm" "-enc-mode 0 -q %%i" SVT-VP9 encmode0
+for /L %%i in (50,-3,26) do call %benchmark_bat% "%~1" "%~n1_SVT-VP9_encmode0_q%%i.webm" "-enc-mode 0 -intra-period 63 -q %%i" SVT-VP9 encmode0
 shift
 goto loop
 :end
