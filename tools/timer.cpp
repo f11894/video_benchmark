4f817ad8819c3df6d57fc072aadb9a04c01797b4
fg118942 <ekusuy-s@jcom.home.ne.jp>
2019-07-04 19:34:32 +0900

    timerをアップデート
    引数の中にスペースなどが含まれていた場合のみダブルクオーテーションで囲むように
    コマンドラインを最初に表示するように
    処理時間の前にTotalMilliseconds : を付けて分かりやすくする

--- tools/timer.cpp
+++ tools/timer.cpp
@@ -5,15 +5,25 @@
   int main(int argc, char *argv[]){
     if(argc < 2) return 0;
     std::stringstream ss;
-    ss << "\"";
-    for(int i = 1; i < argc; ++i)
-      ss << "\"" << argv[i] << "\"" << " ";
-    ss << "\"";
+    std::stringstream ss2;
+    std::string str_arg;
+    for(int i = 1; i < argc; i++)
+      if (strstr(argv[i], " ") != NULL) {
+         ss << "\"" << argv[i] << "\"" << " ";
+      } else if (strstr(argv[i], "&") != NULL) {
+         ss << "\"" << argv[i] << "\"" << " ";
+      } else if (strstr(argv[i], "^") != NULL) {
+         ss << "\"" << argv[i] << "\"" << " ";
+      } else {
+      	 ss << argv[i] << " ";
+      }
+    std::cout << ss.str().c_str() << "\n" << std::endl;
     auto startTime = std::chrono::system_clock::now();
-    system(ss.str().c_str());
+    ss2 << "\"" << ss.str().c_str() << "\"";
+    system(ss2.str().c_str());
     auto endTime = std::chrono::system_clock::now();
     auto timeSpan = endTime - startTime;
-    std::cout << std::chrono::duration_cast<std::chrono::seconds>(timeSpan).count() << std::endl;
+    std::cout << "TotalMilliseconds : " << std::chrono::duration_cast<std::chrono::milliseconds>(timeSpan).count() << std::endl;
     return 0;
   }
   
