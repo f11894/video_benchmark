8fb010030b2fe4e74d9f3adf5f578f5800f79175
fg118942 <ekusuy-s@jcom.home.ne.jp>
2019-07-12 18:59:18 +0900

    timerをutf8に対応させる

--- tools/timer.cpp
+++ tools/timer.cpp
@@ -1,29 +1,31 @@
+  #include <locale>
   #include <chrono>
   #include <cstdlib>
   #include <iostream>
   #include <sstream>
-  int main(int argc, char *argv[]){
+  int wmain(int argc, wchar_t *argv[]){
     if(argc < 2) return 0;
-    std::stringstream ss;
-    std::stringstream ss2;
+    setlocale( LC_ALL, "en_US.UTF-8" );
+    std::wstringstream ss;
+    std::wstringstream ss2;
     std::string str_arg;
     for(int i = 1; i < argc; i++)
-      if (strstr(argv[i], " ") != NULL) {
-         ss << "\"" << argv[i] << "\"" << " ";
-      } else if (strstr(argv[i], "&") != NULL) {
-         ss << "\"" << argv[i] << "\"" << " ";
-      } else if (strstr(argv[i], "^") != NULL) {
-         ss << "\"" << argv[i] << "\"" << " ";
+      if (wcsstr(argv[i], L" ") != NULL) {
+         ss << L"\"" << argv[i] << L"\"" << L" ";
+      } else if (wcsstr(argv[i], L"&") != NULL) {
+         ss << L"\"" << argv[i] << L"\"" << L" ";
+      } else if (wcsstr(argv[i], L"^") != NULL) {
+         ss << L"\"" << argv[i] << L"\"" << L" ";
       } else {
-      	 ss << argv[i] << " ";
+      	 ss << argv[i] << L" ";
       }
-    std::cout << ss.str().c_str() << "\n" << std::endl;
+    std::wcout << ss.str().c_str() << L"\n" << std::endl;
     auto startTime = std::chrono::system_clock::now();
-    ss2 << "\"" << ss.str().c_str() << "\"";
-    system(ss2.str().c_str());
+    ss2 << L"\"" << ss.str().c_str() << L"\"";
+    _wsystem(ss2.str().c_str());
     auto endTime = std::chrono::system_clock::now();
     auto timeSpan = endTime - startTime;
-    std::cout << "TotalMilliseconds : " << std::chrono::duration_cast<std::chrono::milliseconds>(timeSpan).count() << std::endl;
+    std::wcout << L"TotalMilliseconds : " << std::chrono::duration_cast<std::chrono::milliseconds>(timeSpan).count() << std::endl;
     return 0;
   }
   
