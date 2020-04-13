e0fedf57d901de66ae4e14af290172ab2c85c4a8
fg118942 <16046279+f11894@users.noreply.github.com>
2020-04-13 20:20:53 +0900

    日付の削除

--- preset/x26x_libvpx_libaom_2pass_bitrate1000-6000.bat
+++ preset/x26x_libvpx_libaom_2pass_bitrate1000-6000.bat
@@ -2,7 +2,7 @@
 for %%i in ("%~dp0.") do set benchmark_bat="%%~dpivideo_benchmark.bat"
 :loop
 if "%~1"=="" goto end
-for /L %%i in (1000,1000,6000) do call %benchmark_bat% -codec rav1e -i "%~1" -o "%~n1_rav1e_20190921_s2_tunepsnr_2pass_%%ikbps.mp4" -cmd "--bitrate %%i --second-pass rav1e_stats.log --speed 2 --tune psnr --keyint 250 --threads 8 --tile-cols 2 --tile-rows 2" -csvsuf 20190921_s2_tunepsnr_2pass_bitrate
+for /L %%i in (1000,1000,6000) do call %benchmark_bat% -codec rav1e -i "%~1" -o "%~n1_rav1e_s2_tunepsnr_2pass_%%ikbps.mp4" -cmd "--bitrate %%i --second-pass rav1e_stats.log --speed 2 --tune psnr --keyint 250 --threads 8 --tile-cols 2 --tile-rows 2" -csvsuf 20190921_s2_tunepsnr_2pass_bitrate
 for /L %%i in (1000,1000,6000) do call %benchmark_bat% -codec libaom -i "%~1" -o "%~n1_libaom_c1_%%ikbps.mp4" -cmd "--ivf --cpu-used=1 --threads=8 --tile-columns=2 --tile-rows=2 --pass=2 --passes=2  --kf-max-dist=250 --end-usage=vbr --target-bitrate=%%i" -csvsuf c1_2pass_bitrate
 for /L %%i in (1000,1000,6000) do call %benchmark_bat% -codec x264 -i "%~1" -o "%~n1_x264_veryslow_tunessim_%%ikbps.mp4" -cmd "--preset veryslow --tune ssim --keyint 250 --bitrate %%i --pass 2" -csvsuf veryslow_tunessim_2pass_bitrate
 for /L %%i in (1000,1000,6000) do call %benchmark_bat% -codec x265 -i "%~1" -o "%~n1_x265_veryslow_tunessim_%%ikbps.mp4" -cmd "--preset veryslow --tune ssim --keyint 250 --bitrate %%i --pass 2" -csvsuf veryslow_tunessim_2pass_bitrate
--- preset/x26x_libvpx_libaom_crf.bat
+++ preset/x26x_libvpx_libaom_crf.bat
@@ -2,7 +2,7 @@
 for %%i in ("%~dp0.") do set benchmark_bat="%%~dpivideo_benchmark.bat"
 :loop
 if "%~1"=="" goto end
-for /L %%i in (160,-20,60) do call %benchmark_bat% -codec rav1e -i "%~1" -o "%~n1_rav1e_20190921_s2_tunepsnr_1pass_q%%i.mp4" -cmd "--quantizer %%i --speed 2 --tune psnr --keyint 250 --threads 8 --tile-cols 2 --tile-rows 2" -csvsuf 20190921_s2_tunepsnr_1pass_q
+for /L %%i in (160,-20,60) do call %benchmark_bat% -codec rav1e -i "%~1" -o "%~n1_rav1e_s2_tunepsnr_1pass_q%%i.mp4" -cmd "--quantizer %%i --speed 2 --tune psnr --keyint 250 --threads 8 --tile-cols 2 --tile-rows 2" -csvsuf 20190921_s2_tunepsnr_1pass_q
 for /L %%i in (32,-2,18) do call %benchmark_bat% -codec x264 -i "%~1" -o "%~n1_x264_veryslow_tunessim_crf%%i.mp4" -cmd "--preset veryslow --tune ssim --keyint 250 --crf %%i" -csvsuf veryslow_tunessim_crf
 for /L %%i in (32,-2,18) do call %benchmark_bat% -codec x265 -i "%~1" -o "%~n1_x265_veryslow_tunessim_crf%%i.mp4" -cmd "--preset veryslow --tune ssim --keyint 250 --crf %%i" -csvsuf veryslow_tunessim_crf
 for /L %%i in (55,-5,25) do call %benchmark_bat% -codec libvpx -i "%~1" -o "%~n1_libvpx_vp9_c0_2pass_q%%i.webm" -cmd "--codec=vp9 --webm --good --cpu-used=0 --tune=psnr --threads=8 --tile-columns=2 --tile-rows=2 --pass=2 --passes=2 --auto-alt-ref=6 --kf-max-dist=250 --end-usage=q --cq-level=%%i " -csvsuf vp9_c0_2pass_q
