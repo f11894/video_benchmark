c9eea5a38d46dbea5961316fc68dc838571faaa1
fg118942 <16046279+f11894@users.noreply.github.com>
2020-04-22 21:06:40 +0900

    SVT系のプリセットを変更
    encmode1を基本に
    SVT-AV1の2passエンコード用の設定を追加
    Q値の間隔を3から4に変更

--- preset/SVT_q.bat
+++ preset/SVT_q.bat
@@ -2,9 +2,14 @@
 for %%i in ("%~dp0.") do set benchmark_bat="%%~dpivideo_benchmark.bat"
 :loop
 if "%~1"=="" goto end
-for /L %%i in (32,-2,18) do call %benchmark_bat% -codec SVT-HEVC -i "%~1" -o "%~n1_SVT-HEVC_encmode0_q%%i.mp4" -cmd "-encMode 0 -profile 1 -q %%i" -csvsuf encmode0
-for /L %%i in (50,-3,26) do call %benchmark_bat% -codec SVT-AV1 -i "%~1" -o "%~n1_SVT-AV1_encmode0_q%%i.mp4" -cmd "-enc-mode 0 -intra-period 63 -q %%i" -csvsuf encmode0
-for /L %%i in (50,-3,26) do call %benchmark_bat% -codec SVT-VP9 -i "%~1" -o "%~n1_SVT-VP9_encmode0_q%%i.webm" -cmd "-enc-mode 0 -intra-period 63 -q %%i" -csvsuf encmode0
+for /L %%i in (32,-2,18) do call %benchmark_bat% -codec SVT-HEVC -i "%~1" -o "%~n1_SVT-HEVC_encmode1_q%%i.mp4" -cmd "-encMode 1 -profile 1 -q %%i" -csvsuf encmode1_q
+for /L %%i in (50,-4,26) do call %benchmark_bat% -codec SVT-VP9 -i "%~1" -o "%~n1_SVT-VP9_encmode1_q%%i.webm" -cmd "-enc-mode 1 -intra-period 63 -q %%i" -csvsuf encmode1_q
+
+for /L %%i in (50,-4,26) do (
+    call %benchmark_bat% -codec SVT-AV1 -i "%~1" -o "%~n1_SVT-AV1_encmode5_1pass_q%%i.mp4" -cmd "-enc-mode 5 -output-stat-file svtav1_q%%i.stat -enc-mode-2p 1 -intra-period 63 -q %%i" -csvsuf encmode5_1pass_q
+    call %benchmark_bat% -codec SVT-AV1 -i "%~1" -o "%~n1_SVT-AV1_encmode1_2pass_q%%i.mp4" -cmd "-enc-mode 1 -input-stat-file svtav1_q%%i.stat -intra-period 63 -q %%i" -csvsuf encmode1_2pass_q
+    del "%~dp1svtav1_q%%i.stat"
+)
 shift
 goto loop
 :end
