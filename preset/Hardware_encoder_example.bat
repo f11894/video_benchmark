5cd1c2a78304a08f9d89a4c4c4e64aa24506a8c5
fg118942 <16046279+f11894@users.noreply.github.com>
2020-05-02 14:56:58 +0900

    細かい修正

--- preset/Hardware_encoder_example.bat
+++ preset/Hardware_encoder_example.bat
@@ -10,8 +10,8 @@ rem QSVEncC
 rem for /L %%H in (44,-4,20) do call %benchmark_bat% -codec QSVEncC -i "%~1" -o "%~n1_QSVEncC_best_h264_laicq%%H.mp4" -cmd "--codec h264 --quality best --la-icq %%H" -csvsuf best_h264_laicq
 
 rem For comparison
-for /L %%i in (32,-3,18) do call %benchmark_bat% -codec x264 -i "%~1" -o "%~n1_x264_medium_tunessim_crf%%i.mp4" -cmd "--preset medium --tune ssim --keyint 250 --crf %%i" -csvsuf medium_tunessim_crf
-for /L %%i in (32,-3,18) do call %benchmark_bat% -codec x265 -i "%~1" -o "%~n1_x265_medium_tunessim_crf%%i.mp4" -cmd "--preset medium --tune ssim --keyint 250 --crf %%i" -csvsuf medium_tunessim_crf
+for /L %%i in (32,-2,18) do call %benchmark_bat% -codec x264 -i "%~1" -o "%~n1_x264_medium_tunessim_crf%%i.mp4" -cmd "--preset medium --tune ssim --keyint 250 --crf %%i" -csvsuf medium_tunessim_crf
+for /L %%i in (32,-2,18) do call %benchmark_bat% -codec x265 -i "%~1" -o "%~n1_x265_medium_tunessim_crf%%i.mp4" -cmd "--preset medium --tune ssim --keyint 250 --crf %%i" -csvsuf medium_tunessim_crf
 shift
 goto loop
 :end
