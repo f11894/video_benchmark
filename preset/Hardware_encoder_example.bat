2359e7674cbd6779c0503c6855b0aab40126d083
fg118942 <16046279+f11894@users.noreply.github.com>
2021-03-13 10:11:17 +0900

    プリセット内容を変更
    NVEncC用の設定を追加
    omparePixelFormatをyuv420p10leに

--- preset/Hardware_encoder_example.bat
+++ preset/Hardware_encoder_example.bat
@@ -1,5 +1,7 @@
 @echo off
 for %%i in ("%~dp0.") do set benchmark_bat="%%~dpivideo_benchmark.bat"
+set ComparePixelFormat=-pix_fmt yuv420p10le
+
 :loop
 if "%~1"=="" goto end
 rem VCEEncC
@@ -7,7 +9,16 @@ rem for /L %%i in (36,-3,18) do call %benchmark_bat% -codec VCEEncC -i "%~1" -o 
 rem for /L %%i in (36,-3,18) do call %benchmark_bat% -codec VCEEncC -i "%~1" -o "%~n1_VCEEncC_slow_h264_cqp%%i.mp4" -cmd "--codec h264 --quality slow --cqp %%i" -csvsuf slow_h264_cqp
 
 rem QSVEncC
-rem for /L %%H in (44,-4,20) do call %benchmark_bat% -codec QSVEncC -i "%~1" -o "%~n1_QSVEncC_best_h264_laicq%%H.mp4" -cmd "--codec h264 --quality best --la-icq %%H" -csvsuf best_h264_laicq
+rem for /L %%H in (44,-4,20) do call %benchmark_bat% -codec QSVEncC -i "%~1" -o "%~n1_QSVEncC_best_h264_laicq%%H.mp4" -cmd "--la-icq %%H --la-depth 60 -u 1" -csvsuf best_h264_laicq
+rem for /L %%H in (44,-4,20) do call %benchmark_bat% -codec QSVEncC -i "%~1" -o "%~n1_QSVEncC_best_hevc_laicq%%H.mp4" -cmd "--icq %%i -u 1 -c hevc" -csvsuf best_hevc_laicq
+rem for /L %%H in (44,-4,20) do call %benchmark_bat% -codec QSVEncC -i "%~1" -o "%~n1_QSVEncC_10bit_best_hevc_laicq%%H.mp4" -cmd "--icq %%i -u 1 -c hevc --profile main10 --output-depth 10" -csvsuf 10bit_best_hevc_laicq -encode-depth 10
+
+rem NVEncC
+rem for /L %%H in (44,-4,20) do call %benchmark_bat% -codec NVEncC -i "%~1" -o "%~n1_NVEncC_h264_q%%H.mp4" -cmd "--vbrhq 0 --vbr-quality %%i --preset quality --weightp --bref-mode each --lookahead 32 --level 5.2" -csvsuf h264_q
+rem for /L %%H in (44,-4,20) do call %benchmark_bat% -codec NVEncC -i "%~1" -o "%~n1_NVEncC_hevc_q%%H.mp4" -cmd "--vbrhq 0 --vbr-quality %%i --preset quality --weightp --bref-mode each --lookahead 32 -c hevc --level 6" -csvsuf hevc_q
+rem for /L %%H in (44,-4,20) do call %benchmark_bat% -codec NVEncC -i "%~1" -o "%~n1_NVEncC_10bit_hevc_q%%H.mp4" -cmd "--vbrhq 0 --vbr-quality %%i --preset quality --weightp --bref-mode each --lookahead 32 -c hevc --level 6 --output-depth 10" -csvsuf 10bit_hevc_q -encode-depth 10
+rem for /L %%H in (44,-4,20) do call %benchmark_bat% -codec NVEncC -i "%~1" -o "%~n1_NVEncC_hevc_Bframes_q%%H.mp4" -cmd "--vbrhq 0 --vbr-quality %%i --preset quality --weightp --bref-mode each --lookahead 32 -c hevc --level 6 -b 3" -csvsuf hevc_Bframes_q
+rem for /L %%H in (44,-4,20) do call %benchmark_bat% -codec NVEncC -i "%~1" -o "%~n1_NVEncC_10bit_hevc_Bframes_q%%H.mp4" -cmd "--vbrhq 0 --vbr-quality %%i --preset quality --weightp --bref-mode each --lookahead 32 -c hevc --level 6 --output-depth 10 -b 3" -csvsuf 10bit_hevc_Bframes_q -encode-depth 10
 
 rem For comparison
 for /L %%i in (32,-2,18) do call %benchmark_bat% -codec x264 -i "%~1" -o "%~n1_x264_medium_tunessim_crf%%i.mp4" -cmd "--preset medium --tune ssim --keyint 250 --crf %%i" -csvsuf medium_tunessim_crf
