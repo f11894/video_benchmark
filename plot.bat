62c7933e41207994fc1adc85c5c1dcb9d68a8bf4
fg118942 <16046279+f11894@users.noreply.github.com>
2021-05-15 07:59:35 +0900

    複数のフォルダからplotを連続作成出来るように変更

--- plot.bat
+++ plot.bat
@@ -1,8 +1,14 @@
 @echo off
+set plot="%~dp0tools\plot.exe"
 rem Drag and drop the benchmark_log folder
-
-cd "%~1"
 echo Make plot
-"%~dp0tools\plot.exe" "%~1"
+:start
+if "%~1"=="" goto end
+cd "%~1"
+%plot% "%~1"
+shift
+goto start
+:end
 echo Plot making finished
 pause
+exit /b
