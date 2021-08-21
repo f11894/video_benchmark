a65cae56a19e2cfbccb6e2919544915e33ebbc5c
fg118942 <16046279+f11894@users.noreply.github.com>
2021-08-21 10:51:01 +0900

    変数のセット位置を変える

--- preset/x265_all_presets.bat
+++ preset/x265_all_presets.bat
@@ -1,8 +1,8 @@
 @echo off
 for %%i in ("%~dp0.") do set benchmark_bat="%%~dpivideo_benchmark.bat"
-set num=1
 :loop
 if "%~1"=="" goto end
+set num=1
 for %%a in (ultrafast,superfast,veryfast,faster,fast,medium,slow,slower,veryslow,veryslow) do (
    for /L %%i in (32,-2,18) do call %benchmark_bat% -codec x265 -i "%~1" -o "%~n1_x265_%%num%%_%%a_tunessim_crf%%i.mp4" -cmd "--preset %%a --tune ssim --crf %%i" -csvsuf %%num%%_%%a_tunessim_crf
    set /a num+=1
